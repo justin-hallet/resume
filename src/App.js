93ef8e53c845750a1141b0a5cba04a51afada60f
Justin Hallet <justin.hallet@autodesk.com>
2021-11-26 11:33:00 +1100

    Add jobs/projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import {
+  Timeline,
+  Events,
+  UrlButton,
+  ImageEvent,
+  TextEvent,
+  YouTubeEvent,
+} from '@merc/react-timeline';
+
+import * as moldflow from './Moldflow/projects';
+
+console.log(JSON.stringify(moldflow));
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Justin Hallet
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Timeline>
+      <Events>
+      <ImageEvent
+          date="1990"
+          text="GEC Marconi Avionics"
+          src="https://cdn.rochesteravionicarchives.co.uk/img/catalog/GEC-Marconi_Avionics.jpg"
+          alt="GEC Marconi Avionics"
+          credit="Software Engineer"
+        />
+          <ImageEvent
+          date="1993"
+          text="Special Telephone Systems"
+          src=""
+          alt=""
+          credit="Software Engineer"
+        />
+        <ImageEvent
+          date="1995"
+          text="Moldflow"
+          src="https://cdn.worldvectorlogo.com/logos/moldflow.svg"
+          alt="Moldflow"
+          credit="Director Software Engineering"
+        />
+
+        {moldflow.map((project)=> {
+          return <TextEvent date={project.year.toString()} text={project.name}/>  
+        })}
+
+        <ImageEvent
+          date="2008"
+          text="Autodesk"
+          src="https://brand.autodesk.com/app/uploads/2021/04/alternate-logo-1.svg"
+          alt="Autodesk"
+          credit="Senior Software Architects"
+        />
+
+      </Events>
+    </Timeline>
     </div>
   );
 }
---
+++ src/Moldflow/projects.js
@@ -0,0 +1,18 @@
+var projects = [
+    require("./dynamic-series.json"),
+    require("./ideas.json"),
+    require("./part-adviser.json"),
+    require("./pro-plastics-advisor.json"),
+    require("./solidworks-moldflow-express.json"),
+    require("./impa.json"),
+    require("./plasticszone.json"),
+    require("./mold-adviser.json"),
+    require("./shotscope.json"),
+    require("./matrix-altanium.json"),
+    require("./simalink.json"),
+    require("./synergy.json"),
+    require("./communicator.json"),
+    require("./plastics-adviser.json"),
+    require("./network-license-manager.json"),    
+]
+module.exports=projects
